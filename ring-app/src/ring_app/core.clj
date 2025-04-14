2b912e0c684153b27c33eb59ee6789407909b14d
Eric Ross <epr@rossmbox.com>
2025-04-13 20:18:14 -0700

    Auto reload added

--- ring-app/src/ring_app/core.clj
+++ ring-app/src/ring_app/core.clj
@@ -1,5 +1,7 @@
 (ns ring-app.core
-  (:require [ring.adapter.jetty :as jetty]))
+  (:require 
+   [ring.adapter.jetty :as jetty]
+   [ring.middleware.reload :refer [wrap-reload]]))
 
 
 (defn handler
@@ -7,12 +9,22 @@
   [request-map]
   {:status 200
    :headers {"Content-Type" "text/html"}
-   :body (str "<html><body> your IP is: "
+   :body (str "<html><body> your IP is really: "
          (:remote-addr request-map)
    "</body></html>")})
 
+(defn wrap-nocache [handler]
+  (fn [request]
+    (-> request
+        handler 
+        (assoc-in [:headers "Pragma"] "no-cache")))
+)
+
 
 (defn -main []
-  (jetty/run-jetty handler
+  (jetty/run-jetty (-> handler 
+                       var
+                       wrap-nocache
+                       wrap-reload)
                    {:port 3000
                     :join? false}))
