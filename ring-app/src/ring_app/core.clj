17756ffe415ff586c4c575a99a8c4a792cb018aa
Eric Ross <epr@rossmbox.com>
2025-04-15 18:38:55 -0700

    Added json

--- ring-app/src/ring_app/core.clj
+++ ring-app/src/ring_app/core.clj
@@ -1,11 +1,12 @@
 (ns ring-app.core
   (:require 
+   [muuntaja.middleware :as muuntaja]
    [ring.adapter.jetty :as jetty]
    [ring.util.http-response :as response]
    [ring.middleware.reload :refer [wrap-reload]]))
 
 
-(defn handler [request-map]
+(defn html-handler [request-map]
   "A simple Ring handler that returns a greeting."
   (response/ok
    (str "<html><body> your IP is really here: "
@@ -19,10 +20,20 @@
         (assoc-in [:headers "Pragma"] "no-cache")))
 )
 
+(defn json-handler [request]
+  (response/ok
+   {:result (get-in request [:body-params :id])}))
+
+(def handler json-handler)
+
+(defn wrap-formats [handler]
+  (-> handler
+      (muuntaja/wrap-format)))
 
 (defn -main []
   (jetty/run-jetty (-> #'handler
                        wrap-nocache
+                       wrap-formats
                        wrap-reload)
                    {:port 3000
                     :join? false}))
